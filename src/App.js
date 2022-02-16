7a3eb5ac78c4c76a5300b210a4bbd61099448b7f
kartikpavan <kartik.varanasi@yahoo.com>
2022-02-16 17:37:20 +0530

    tailwind styles applied

--- src/App.js
+++ src/App.js
@@ -51,18 +51,29 @@ function App() {
     localStorage.setItem("list", JSON.stringify(list));
   }, [list]);
 
+  //! Todays date
+  let options = { weekday: "short", month: "short", day: "numeric" };
+
   return (
-    <div>
+    <div className=" w-2/6 mt-20 p-4 mx-auto text-center text-gray-200 rounded-xl shadow bg-gradient-to-br from-purple-600 to-blue-500 ">
       <header>
-        <h1>Todo App</h1>
-        <form onSubmit={submitHandler}>
+        <h1 className="text-3xl font-bold mt-1 mb-6 ">
+          Todo App ({new Date().toLocaleDateString("en-us", options)})
+        </h1>
+        <form onSubmit={submitHandler} className="flex justify-center ">
           <input
+            className="w-60 h-10 rounded-lg mx-2 text-gray-700 font-semibold text-xl"
             type="text"
             name="task"
             value={todo.task}
             onChange={(e) => setTodo({ ...todo, task: e.target.value })}
           />
-          <button type="submit"> Add</button>
+          <button
+            type="submit"
+            className=" text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 dark:border-gray-700"
+          >
+            ADD
+          </button>
         </form>
       </header>
       {list.length > 0 ? (
@@ -72,7 +83,13 @@ function App() {
             removeTodo={removeTodo}
             toggleComplete={toggleComplete}
           ></List>
-          <button onClick={() => setList([])}>Clear all</button>
+          <button
+            // className="bg-red-800 w-full rounded-lg py-1 text-lg mt-4 "
+            class="py-2.5 px-5 mr-2 w-full mt-4 mb-0 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            onClick={() => setList([])}
+          >
+            Clear ALL
+          </button>
         </div>
       ) : null}
     </div>
